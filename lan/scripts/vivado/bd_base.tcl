b1cdbc3fe2516eacc07c9b6c698ce00f5b4e75f7
Kris Provoost <provoostkris@hotmail.com>
2024-06-19 22:46:28 +0200

    adding base block design

---
+++ lan/scripts/vivado/bd_base.tcl
@@ -0,0 +1,397 @@
+
+################################################################
+# This is a generated script based on design: design_1
+#
+# Though there are limitations about the generated script,
+# the main purpose of this utility is to make learning
+# IP Integrator Tcl commands easier.
+################################################################
+
+namespace eval _tcl {
+proc get_script_folder {} {
+   set script_path [file normalize [info script]]
+   set script_folder [file dirname $script_path]
+   return $script_folder
+}
+}
+variable script_folder
+set script_folder [_tcl::get_script_folder]
+cd $script_folder
+pwd
+cd ..
+cd ..
+pwd
+
+################################################################
+# Check if script is running in correct Vivado version.
+################################################################
+set scripts_vivado_version 2023.1
+set current_vivado_version [version -short]
+
+if { [string first $scripts_vivado_version $current_vivado_version] == -1 } {
+   puts ""
+   catch {common::send_gid_msg -ssname BD::TCL -id 2041 -severity "ERROR" "This script was generated using Vivado <$scripts_vivado_version> and is being run in <$current_vivado_version> of Vivado. Please run the script in Vivado <$scripts_vivado_version> then open the design in Vivado <$current_vivado_version>. Upgrade the design by running \"Tools => Report => Report IP Status...\", then run write_bd_tcl to create an updated script."}
+
+   return 1
+}
+
+################################################################
+# START
+################################################################
+
+# To test this script, run the following commands from Vivado Tcl console:
+# source design_1_script.tcl
+
+# If there is no project opened, this script will create a
+# project, but make sure you do not have an existing project
+# <./myproj/project_1.xpr> in the current working folder.
+
+set list_projs [get_projects -quiet]
+if { $list_projs eq "" } {
+   create_project prj_bd_base bd_base -part xc7z020clg400-2
+
+   set_property target_language VHDL [current_project]
+   set_property simulator_language VHDL [current_project]
+}
+
+
+# CHANGE DESIGN NAME HERE
+variable design_name
+set design_name bd_base
+
+# If you do not already have an existing IP Integrator design open,
+# you can create a design using the following command:
+#    create_bd_design $design_name
+
+# Creating design if needed
+set errMsg ""
+set nRet 0
+
+set cur_design [current_bd_design -quiet]
+set list_cells [get_bd_cells -quiet]
+
+if { ${design_name} eq "" } {
+   # USE CASES:
+   #    1) Design_name not set
+
+   set errMsg "Please set the variable <design_name> to a non-empty value."
+   set nRet 1
+
+} elseif { ${cur_design} ne "" && ${list_cells} eq "" } {
+   # USE CASES:
+   #    2): Current design opened AND is empty AND names same.
+   #    3): Current design opened AND is empty AND names diff; design_name NOT in project.
+   #    4): Current design opened AND is empty AND names diff; design_name exists in project.
+
+   if { $cur_design ne $design_name } {
+      common::send_gid_msg -ssname BD::TCL -id 2001 -severity "INFO" "Changing value of <design_name> from <$design_name> to <$cur_design> since current design is empty."
+      set design_name [get_property NAME $cur_design]
+   }
+   common::send_gid_msg -ssname BD::TCL -id 2002 -severity "INFO" "Constructing design in IPI design <$cur_design>..."
+
+} elseif { ${cur_design} ne "" && $list_cells ne "" && $cur_design eq $design_name } {
+   # USE CASES:
+   #    5) Current design opened AND has components AND same names.
+
+   set errMsg "Design <$design_name> already exists in your project, please set the variable <design_name> to another value."
+   set nRet 1
+} elseif { [get_files -quiet ${design_name}.bd] ne "" } {
+   # USE CASES:
+   #    6) Current opened design, has components, but diff names, design_name exists in project.
+   #    7) No opened design, design_name exists in project.
+
+   set errMsg "Design <$design_name> already exists in your project, please set the variable <design_name> to another value."
+   set nRet 2
+
+} else {
+   # USE CASES:
+   #    8) No opened design, design_name not in project.
+   #    9) Current opened design, has components, but diff names, design_name not in project.
+
+   common::send_gid_msg -ssname BD::TCL -id 2003 -severity "INFO" "Currently there is no design <$design_name> in project, so creating one..."
+
+   create_bd_design $design_name
+
+   common::send_gid_msg -ssname BD::TCL -id 2004 -severity "INFO" "Making design <$design_name> as current_bd_design."
+   current_bd_design $design_name
+
+}
+
+common::send_gid_msg -ssname BD::TCL -id 2005 -severity "INFO" "Currently the variable <design_name> is equal to \"$design_name\"."
+
+if { $nRet != 0 } {
+   catch {common::send_gid_msg -ssname BD::TCL -id 2006 -severity "ERROR" $errMsg}
+   return $nRet
+}
+
+set bCheckIPsPassed 1
+##################################################################
+# CHECK IPs
+##################################################################
+set bCheckIPs 1
+if { $bCheckIPs == 1 } {
+   set list_check_ips "\
+xilinx.com:ip:processing_system7:5.5\
+xilinx.com:ip:proc_sys_reset:5.0\
+xilinx.com:ip:axi_gpio:2.0\
+"
+
+   set list_ips_missing ""
+   common::send_gid_msg -ssname BD::TCL -id 2011 -severity "INFO" "Checking if the following IPs exist in the project's IP catalog: $list_check_ips ."
+
+   foreach ip_vlnv $list_check_ips {
+      set ip_obj [get_ipdefs -all $ip_vlnv]
+      if { $ip_obj eq "" } {
+         lappend list_ips_missing $ip_vlnv
+      }
+   }
+
+   if { $list_ips_missing ne "" } {
+      catch {common::send_gid_msg -ssname BD::TCL -id 2012 -severity "ERROR" "The following IPs are not found in the IP Catalog:\n  $list_ips_missing\n\nResolution: Please add the repository containing the IP(s) to the project." }
+      set bCheckIPsPassed 0
+   }
+
+}
+
+if { $bCheckIPsPassed != 1 } {
+  common::send_gid_msg -ssname BD::TCL -id 2023 -severity "WARNING" "Will not continue with creation of design due to the error(s) above."
+  return 3
+}
+
+##################################################################
+# DESIGN PROCs
+##################################################################
+
+
+
+# Procedure to create entire design; Provide argument to make
+# procedure reusable. If parentCell is "", will use root.
+proc create_root_design { parentCell } {
+
+  variable script_folder
+  variable design_name
+
+  if { $parentCell eq "" } {
+     set parentCell [get_bd_cells /]
+  }
+
+  # Get object for parentCell
+  set parentObj [get_bd_cells $parentCell]
+  if { $parentObj == "" } {
+     catch {common::send_gid_msg -ssname BD::TCL -id 2090 -severity "ERROR" "Unable to find parent cell <$parentCell>!"}
+     return
+  }
+
+  # Make sure parentObj is hier blk
+  set parentType [get_property TYPE $parentObj]
+  if { $parentType ne "hier" } {
+     catch {common::send_gid_msg -ssname BD::TCL -id 2091 -severity "ERROR" "Parent <$parentObj> has TYPE = <$parentType>. Expected to be <hier>."}
+     return
+  }
+
+  # Save current instance; Restore later
+  set oldCurInst [current_bd_instance .]
+
+  # Set parent object as current
+  current_bd_instance $parentObj
+
+
+  # Create interface ports
+  set led [ create_bd_intf_port -mode Master -vlnv xilinx.com:interface:gpio_rtl:1.0 led ]
+
+  set key [ create_bd_intf_port -mode Master -vlnv xilinx.com:interface:gpio_rtl:1.0 key ]
+
+  set DDR [ create_bd_intf_port -mode Master -vlnv xilinx.com:interface:ddrx_rtl:1.0 DDR ]
+
+  set FIXED_IO [ create_bd_intf_port -mode Master -vlnv xilinx.com:display_processing_system7:fixedio_rtl:1.0 FIXED_IO ]
+
+
+  # Create ports
+
+  # Create instance: processing_system7_0, and set properties
+  set processing_system7_0 [ create_bd_cell -type ip -vlnv xilinx.com:ip:processing_system7:5.5 processing_system7_0 ]
+  set_property -dict [list \
+    CONFIG.PCW_ACT_APU_PERIPHERAL_FREQMHZ {666.666687} \
+    CONFIG.PCW_ACT_CAN_PERIPHERAL_FREQMHZ {10.000000} \
+    CONFIG.PCW_ACT_DCI_PERIPHERAL_FREQMHZ {10.158730} \
+    CONFIG.PCW_ACT_ENET0_PERIPHERAL_FREQMHZ {125.000000} \
+    CONFIG.PCW_ACT_ENET1_PERIPHERAL_FREQMHZ {10.000000} \
+    CONFIG.PCW_ACT_FPGA0_PERIPHERAL_FREQMHZ {50.000000} \
+    CONFIG.PCW_ACT_FPGA1_PERIPHERAL_FREQMHZ {10.000000} \
+    CONFIG.PCW_ACT_FPGA2_PERIPHERAL_FREQMHZ {10.000000} \
+    CONFIG.PCW_ACT_FPGA3_PERIPHERAL_FREQMHZ {10.000000} \
+    CONFIG.PCW_ACT_PCAP_PERIPHERAL_FREQMHZ {200.000000} \
+    CONFIG.PCW_ACT_QSPI_PERIPHERAL_FREQMHZ {10.000000} \
+    CONFIG.PCW_ACT_SDIO_PERIPHERAL_FREQMHZ {100.000000} \
+    CONFIG.PCW_ACT_SMC_PERIPHERAL_FREQMHZ {10.000000} \
+    CONFIG.PCW_ACT_SPI_PERIPHERAL_FREQMHZ {10.000000} \
+    CONFIG.PCW_ACT_TPIU_PERIPHERAL_FREQMHZ {200.000000} \
+    CONFIG.PCW_ACT_TTC0_CLK0_PERIPHERAL_FREQMHZ {111.111115} \
+    CONFIG.PCW_ACT_TTC0_CLK1_PERIPHERAL_FREQMHZ {111.111115} \
+    CONFIG.PCW_ACT_TTC0_CLK2_PERIPHERAL_FREQMHZ {111.111115} \
+    CONFIG.PCW_ACT_TTC1_CLK0_PERIPHERAL_FREQMHZ {111.111115} \
+    CONFIG.PCW_ACT_TTC1_CLK1_PERIPHERAL_FREQMHZ {111.111115} \
+    CONFIG.PCW_ACT_TTC1_CLK2_PERIPHERAL_FREQMHZ {111.111115} \
+    CONFIG.PCW_ACT_UART_PERIPHERAL_FREQMHZ {100.000000} \
+    CONFIG.PCW_ACT_WDT_PERIPHERAL_FREQMHZ {111.111115} \
+    CONFIG.PCW_CLK0_FREQ {50000000} \
+    CONFIG.PCW_CLK1_FREQ {10000000} \
+    CONFIG.PCW_CLK2_FREQ {10000000} \
+    CONFIG.PCW_CLK3_FREQ {10000000} \
+    CONFIG.PCW_DDR_RAM_HIGHADDR {0x1FFFFFFF} \
+    CONFIG.PCW_ENET0_ENET0_IO {MIO 16 .. 27} \
+    CONFIG.PCW_ENET0_GRP_MDIO_ENABLE {0} \
+    CONFIG.PCW_ENET0_PERIPHERAL_CLKSRC {IO PLL} \
+    CONFIG.PCW_ENET0_PERIPHERAL_ENABLE {1} \
+    CONFIG.PCW_ENET0_PERIPHERAL_FREQMHZ {1000 Mbps} \
+    CONFIG.PCW_EN_EMIO_ENET0 {0} \
+    CONFIG.PCW_EN_ENET0 {1} \
+    CONFIG.PCW_EN_SDIO0 {1} \
+    CONFIG.PCW_EN_UART1 {1} \
+    CONFIG.PCW_FPGA_FCLK0_ENABLE {1} \
+    CONFIG.PCW_MIO_16_IOTYPE {LVCMOS 1.8V} \
+    CONFIG.PCW_MIO_16_PULLUP {enabled} \
+    CONFIG.PCW_MIO_16_SLEW {slow} \
+    CONFIG.PCW_MIO_17_IOTYPE {LVCMOS 1.8V} \
+    CONFIG.PCW_MIO_17_PULLUP {enabled} \
+    CONFIG.PCW_MIO_17_SLEW {slow} \
+    CONFIG.PCW_MIO_18_IOTYPE {LVCMOS 1.8V} \
+    CONFIG.PCW_MIO_18_PULLUP {enabled} \
+    CONFIG.PCW_MIO_18_SLEW {slow} \
+    CONFIG.PCW_MIO_19_IOTYPE {LVCMOS 1.8V} \
+    CONFIG.PCW_MIO_19_PULLUP {enabled} \
+    CONFIG.PCW_MIO_19_SLEW {slow} \
+    CONFIG.PCW_MIO_20_IOTYPE {LVCMOS 1.8V} \
+    CONFIG.PCW_MIO_20_PULLUP {enabled} \
+    CONFIG.PCW_MIO_20_SLEW {slow} \
+    CONFIG.PCW_MIO_21_IOTYPE {LVCMOS 1.8V} \
+    CONFIG.PCW_MIO_21_PULLUP {enabled} \
+    CONFIG.PCW_MIO_21_SLEW {slow} \
+    CONFIG.PCW_MIO_22_IOTYPE {LVCMOS 1.8V} \
+    CONFIG.PCW_MIO_22_PULLUP {enabled} \
+    CONFIG.PCW_MIO_22_SLEW {slow} \
+    CONFIG.PCW_MIO_23_IOTYPE {LVCMOS 1.8V} \
+    CONFIG.PCW_MIO_23_PULLUP {enabled} \
+    CONFIG.PCW_MIO_23_SLEW {slow} \
+    CONFIG.PCW_MIO_24_IOTYPE {LVCMOS 1.8V} \
+    CONFIG.PCW_MIO_24_PULLUP {enabled} \
+    CONFIG.PCW_MIO_24_SLEW {slow} \
+    CONFIG.PCW_MIO_25_IOTYPE {LVCMOS 1.8V} \
+    CONFIG.PCW_MIO_25_PULLUP {enabled} \
+    CONFIG.PCW_MIO_25_SLEW {slow} \
+    CONFIG.PCW_MIO_26_IOTYPE {LVCMOS 1.8V} \
+    CONFIG.PCW_MIO_26_PULLUP {enabled} \
+    CONFIG.PCW_MIO_26_SLEW {slow} \
+    CONFIG.PCW_MIO_27_IOTYPE {LVCMOS 1.8V} \
+    CONFIG.PCW_MIO_27_PULLUP {enabled} \
+    CONFIG.PCW_MIO_27_SLEW {slow} \
+    CONFIG.PCW_MIO_40_IOTYPE {LVCMOS 1.8V} \
+    CONFIG.PCW_MIO_40_PULLUP {enabled} \
+    CONFIG.PCW_MIO_40_SLEW {slow} \
+    CONFIG.PCW_MIO_41_IOTYPE {LVCMOS 1.8V} \
+    CONFIG.PCW_MIO_41_PULLUP {enabled} \
+    CONFIG.PCW_MIO_41_SLEW {slow} \
+    CONFIG.PCW_MIO_42_IOTYPE {LVCMOS 1.8V} \
+    CONFIG.PCW_MIO_42_PULLUP {enabled} \
+    CONFIG.PCW_MIO_42_SLEW {slow} \
+    CONFIG.PCW_MIO_43_IOTYPE {LVCMOS 1.8V} \
+    CONFIG.PCW_MIO_43_PULLUP {enabled} \
+    CONFIG.PCW_MIO_43_SLEW {slow} \
+    CONFIG.PCW_MIO_44_IOTYPE {LVCMOS 1.8V} \
+    CONFIG.PCW_MIO_44_PULLUP {enabled} \
+    CONFIG.PCW_MIO_44_SLEW {slow} \
+    CONFIG.PCW_MIO_45_IOTYPE {LVCMOS 1.8V} \
+    CONFIG.PCW_MIO_45_PULLUP {enabled} \
+    CONFIG.PCW_MIO_45_SLEW {slow} \
+    CONFIG.PCW_MIO_48_IOTYPE {LVCMOS 1.8V} \
+    CONFIG.PCW_MIO_48_PULLUP {enabled} \
+    CONFIG.PCW_MIO_48_SLEW {slow} \
+    CONFIG.PCW_MIO_49_IOTYPE {LVCMOS 1.8V} \
+    CONFIG.PCW_MIO_49_PULLUP {enabled} \
+    CONFIG.PCW_MIO_49_SLEW {slow} \
+    CONFIG.PCW_MIO_TREE_PERIPHERALS {unassigned#unassigned#unassigned#unassigned#unassigned#unassigned#unassigned#unassigned#unassigned#unassigned#unassigned#unassigned#unassigned#unassigned#unassigned#unassigned#Enet\
+0#Enet 0#Enet 0#Enet 0#Enet 0#Enet 0#Enet 0#Enet 0#Enet 0#Enet 0#Enet 0#Enet 0#unassigned#unassigned#unassigned#unassigned#unassigned#unassigned#unassigned#unassigned#unassigned#unassigned#unassigned#unassigned#SD\
+0#SD 0#SD 0#SD 0#SD 0#SD 0#unassigned#unassigned#UART 1#UART 1#unassigned#unassigned#unassigned#unassigned} \
+    CONFIG.PCW_MIO_TREE_SIGNALS {unassigned#unassigned#unassigned#unassigned#unassigned#unassigned#unassigned#unassigned#unassigned#unassigned#unassigned#unassigned#unassigned#unassigned#unassigned#unassigned#tx_clk#txd[0]#txd[1]#txd[2]#txd[3]#tx_ctl#rx_clk#rxd[0]#rxd[1]#rxd[2]#rxd[3]#rx_ctl#unassigned#unassigned#unassigned#unassigned#unassigned#unassigned#unassigned#unassigned#unassigned#unassigned#unassigned#unassigned#clk#cmd#data[0]#data[1]#data[2]#data[3]#unassigned#unassigned#tx#rx#unassigned#unassigned#unassigned#unassigned}\
+\
+    CONFIG.PCW_PRESET_BANK1_VOLTAGE {LVCMOS 1.8V} \
+    CONFIG.PCW_SD0_GRP_CD_ENABLE {0} \
+    CONFIG.PCW_SD0_GRP_POW_ENABLE {0} \
+    CONFIG.PCW_SD0_GRP_WP_ENABLE {0} \
+    CONFIG.PCW_SD0_PERIPHERAL_ENABLE {1} \
+    CONFIG.PCW_SD0_SD0_IO {MIO 40 .. 45} \
+    CONFIG.PCW_SDIO_PERIPHERAL_FREQMHZ {100} \
+    CONFIG.PCW_SDIO_PERIPHERAL_VALID {1} \
+    CONFIG.PCW_UART1_GRP_FULL_ENABLE {0} \
+    CONFIG.PCW_UART1_PERIPHERAL_ENABLE {1} \
+    CONFIG.PCW_UART1_UART1_IO {MIO 48 .. 49} \
+    CONFIG.PCW_UART_PERIPHERAL_FREQMHZ {100} \
+    CONFIG.PCW_UART_PERIPHERAL_VALID {1} \
+    CONFIG.PCW_UIPARAM_ACT_DDR_FREQ_MHZ {533.333374} \
+    CONFIG.PCW_UIPARAM_DDR_BUS_WIDTH {16 Bit} \
+    CONFIG.PCW_UIPARAM_DDR_ECC {Disabled} \
+    CONFIG.PCW_UIPARAM_DDR_PARTNO {MT41J256M16 RE-125} \
+  ] $processing_system7_0
+
+
+  # Create instance: rst_ps7_0_50M, and set properties
+  set rst_ps7_0_50M [ create_bd_cell -type ip -vlnv xilinx.com:ip:proc_sys_reset:5.0 rst_ps7_0_50M ]
+
+  # Create instance: led, and set properties
+  set led [ create_bd_cell -type ip -vlnv xilinx.com:ip:axi_gpio:2.0 led ]
+  set_property -dict [list \
+    CONFIG.C_ALL_INPUTS {0} \
+    CONFIG.C_ALL_OUTPUTS {1} \
+    CONFIG.C_GPIO_WIDTH {1} \
+    CONFIG.C_INTERRUPT_PRESENT {0} \
+  ] $led
+
+
+  # Create instance: key, and set properties
+  set key [ create_bd_cell -type ip -vlnv xilinx.com:ip:axi_gpio:2.0 key ]
+  set_property -dict [list \
+    CONFIG.C_ALL_INPUTS {1} \
+    CONFIG.C_GPIO_WIDTH {1} \
+  ] $key
+
+
+  # Create instance: ps7_0_axi_periph, and set properties
+  set ps7_0_axi_periph [ create_bd_cell -type ip -vlnv xilinx.com:ip:axi_interconnect:2.1 ps7_0_axi_periph ]
+  set_property CONFIG.NUM_MI {2} $ps7_0_axi_periph
+
+
+  # Create interface connections
+  connect_bd_intf_net -intf_net key_GPIO [get_bd_intf_ports key] [get_bd_intf_pins key/GPIO]
+  connect_bd_intf_net -intf_net led_GPIO [get_bd_intf_ports led] [get_bd_intf_pins led/GPIO]
+  connect_bd_intf_net -intf_net processing_system7_0_DDR [get_bd_intf_ports DDR] [get_bd_intf_pins processing_system7_0/DDR]
+  connect_bd_intf_net -intf_net processing_system7_0_FIXED_IO [get_bd_intf_ports FIXED_IO] [get_bd_intf_pins processing_system7_0/FIXED_IO]
+  connect_bd_intf_net -intf_net processing_system7_0_M_AXI_GP0 [get_bd_intf_pins processing_system7_0/M_AXI_GP0] [get_bd_intf_pins ps7_0_axi_periph/S00_AXI]
+  connect_bd_intf_net -intf_net ps7_0_axi_periph_M00_AXI [get_bd_intf_pins ps7_0_axi_periph/M00_AXI] [get_bd_intf_pins led/S_AXI]
+  connect_bd_intf_net -intf_net ps7_0_axi_periph_M01_AXI [get_bd_intf_pins ps7_0_axi_periph/M01_AXI] [get_bd_intf_pins key/S_AXI]
+
+  # Create port connections
+  connect_bd_net -net processing_system7_0_FCLK_CLK0 [get_bd_pins processing_system7_0/FCLK_CLK0] [get_bd_pins processing_system7_0/M_AXI_GP0_ACLK] [get_bd_pins rst_ps7_0_50M/slowest_sync_clk] [get_bd_pins led/s_axi_aclk] [get_bd_pins key/s_axi_aclk] [get_bd_pins ps7_0_axi_periph/ACLK] [get_bd_pins ps7_0_axi_periph/S00_ACLK] [get_bd_pins ps7_0_axi_periph/M00_ACLK] [get_bd_pins ps7_0_axi_periph/M01_ACLK]
+  connect_bd_net -net processing_system7_0_FCLK_RESET0_N [get_bd_pins processing_system7_0/FCLK_RESET0_N] [get_bd_pins rst_ps7_0_50M/ext_reset_in]
+  connect_bd_net -net rst_ps7_0_50M_peripheral_aresetn [get_bd_pins rst_ps7_0_50M/peripheral_aresetn] [get_bd_pins led/s_axi_aresetn] [get_bd_pins key/s_axi_aresetn] [get_bd_pins ps7_0_axi_periph/ARESETN] [get_bd_pins ps7_0_axi_periph/S00_ARESETN] [get_bd_pins ps7_0_axi_periph/M00_ARESETN] [get_bd_pins ps7_0_axi_periph/M01_ARESETN]
+
+  # Create address segments
+  assign_bd_address -offset 0x41210000 -range 0x00010000 -target_address_space [get_bd_addr_spaces processing_system7_0/Data] [get_bd_addr_segs key/S_AXI/Reg] -force
+  assign_bd_address -offset 0x41200000 -range 0x00010000 -target_address_space [get_bd_addr_spaces processing_system7_0/Data] [get_bd_addr_segs led/S_AXI/Reg] -force
+
+
+  # Restore current instance
+  current_bd_instance $oldCurInst
+
+  validate_bd_design
+  save_bd_design
+}
+# End of create_root_design()
+
+
+##################################################################
+# MAIN FLOW
+##################################################################
+
+create_root_design ""
+
+
