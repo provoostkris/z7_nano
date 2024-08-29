d9dcab4c4e0aed75447b9a852148946312fdf538
Kris Provoost <provoostkris@hotmail.com>
2024-08-29 22:33:36 +0200

    more pin mapping

--- lan/vhdl/eth_frm_rx.vhd
+++ lan/vhdl/eth_frm_rx.vhd
@@ -6,6 +6,7 @@
 library ieee;
 use     ieee.std_logic_1164.all;
 use     ieee.numeric_std.all;
+
 use     work.eth_pkg.all;
 -------------------------------------------------------------------------------
 entity eth_frm_rx is
