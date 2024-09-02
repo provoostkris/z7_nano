45148ce0aabfa6f83947acaf3261a30d83ddd181
Kris Provoost <provoostkris@hotmail.com>
2024-09-02 22:40:31 +0200

    add the LED again

--- lan/bench/tb_lan.vhd
+++ lan/bench/tb_lan.vhd
@@ -109,6 +109,8 @@ dut: entity work.lan(rtl)
     rgmii_tx_ctl      => rgmii_tx_ctl ,
     rgmii_td          => rgmii_td     ,
     phy_rst_n         => phy_rst_n    ,
+    
+    led               => open ,
 
     DDR_addr          => open ,
     DDR_ba            => open ,
