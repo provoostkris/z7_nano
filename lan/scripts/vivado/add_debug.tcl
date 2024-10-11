f9d6036813282112b345add9eb689f1822fa5621
Kris Provoost <provoostkris@hotmail.com>
2024-10-11 22:21:09 +0200

    Update add_debug.tcl

--- lan/scripts/vivado/add_debug.tcl
+++ lan/scripts/vivado/add_debug.tcl
@@ -30,7 +30,7 @@ set_property ALL_PROBE_SAME_MU true [get_debug_cores u_ila_1 ]
 set_property ALL_PROBE_SAME_MU_CNT 4 [get_debug_cores u_ila_1 ]
 endgroup
 connect_debug_port u_ila_0/clk [get_nets [list bd_base_i/processing_system7_0/inst/FCLK_CLK0 ]]
-connect_debug_port u_ila_1/clk [get_nets [list clk_eth ]]
+connect_debug_port u_ila_1/clk [get_nets [list clk_012 ]]
 set_property port_width 32 [get_debug_ports u_ila_0/probe0]
 set_property PROBE_TYPE DATA_AND_TRIGGER [get_debug_ports u_ila_0/probe0]
 connect_debug_port u_ila_0/probe0 [get_nets [list {AXI_STR_TXD_0_tdata[0]} {AXI_STR_TXD_0_tdata[1]} {AXI_STR_TXD_0_tdata[2]} {AXI_STR_TXD_0_tdata[3]} {AXI_STR_TXD_0_tdata[4]} {AXI_STR_TXD_0_tdata[5]} {AXI_STR_TXD_0_tdata[6]} {AXI_STR_TXD_0_tdata[7]} {AXI_STR_TXD_0_tdata[8]} {AXI_STR_TXD_0_tdata[9]} {AXI_STR_TXD_0_tdata[10]} {AXI_STR_TXD_0_tdata[11]} {AXI_STR_TXD_0_tdata[12]} {AXI_STR_TXD_0_tdata[13]} {AXI_STR_TXD_0_tdata[14]} {AXI_STR_TXD_0_tdata[15]} {AXI_STR_TXD_0_tdata[16]} {AXI_STR_TXD_0_tdata[17]} {AXI_STR_TXD_0_tdata[18]} {AXI_STR_TXD_0_tdata[19]} {AXI_STR_TXD_0_tdata[20]} {AXI_STR_TXD_0_tdata[21]} {AXI_STR_TXD_0_tdata[22]} {AXI_STR_TXD_0_tdata[23]} {AXI_STR_TXD_0_tdata[24]} {AXI_STR_TXD_0_tdata[25]} {AXI_STR_TXD_0_tdata[26]} {AXI_STR_TXD_0_tdata[27]} {AXI_STR_TXD_0_tdata[28]} {AXI_STR_TXD_0_tdata[29]} {AXI_STR_TXD_0_tdata[30]} {AXI_STR_TXD_0_tdata[31]} ]]
