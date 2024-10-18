ff66906c4a7d31fbbc26f6774ab77c7457a000b5
Kris Provoost <provoostkris@hotmail.com>
2024-10-18 15:49:45 +0200

    update for compatibility with reduced rate interfaces

--- lan/vhdl/eth_frm_rx.vhd
+++ lan/vhdl/eth_frm_rx.vhd
@@ -14,6 +14,8 @@ entity eth_frm_rx is
     iclk          : in std_logic;
     irst_n        : in std_logic;
 
+    irx_ena       : in std_logic;
+    irx_lst       : in std_logic;
     irxdata       : in std_logic_vector(7 downto 0);
     irxdv         : in std_logic;
     irxer         : in std_logic;
@@ -42,8 +44,6 @@ architecture rtl of eth_frm_rx is
   signal sof, eof      : std_logic;
   signal crcen, crcen2 : std_logic;
 
-  signal dvdly   : std_logic_vector(4 downto 0);
-  signal datadly : t_arr_slv_8;
 
   type state_t is ( idle,
                     sfd,
@@ -56,9 +56,8 @@ architecture rtl of eth_frm_rx is
   signal bytecnt     : unsigned(15 downto 0);
   signal destmacaddr : std_logic_vector(47 downto 0);
 
-  signal rxdv        : std_logic;
   signal dataen      : std_logic;
-
+  signal datadly        : t_arr_slv_8;
   signal crc_calculated : t_arr_slv_32;
   signal crc_is_fine    : std_logic;
 
@@ -66,9 +65,6 @@ begin
 
   oeof    <= eof;
 
-  --! create enable signals
-  rxdv    <= irxdv and dataen and not irxer;    -- when the data is valid
-  crcen2  <= irxdv and crcen  and not irxer;    -- when the data needs checksum
 
   --! register outputs
   process (iclk, irst_n) is
@@ -78,9 +74,29 @@ begin
       orxdv   <= '0';
       orxerr  <= '0';
     elsif rising_edge(iclk) then
-      orxdata <= datadly(datadly'high);
-      orxdv   <= dvdly(dvdly'high) and dataen;
+    if dataen = '1'then
+      orxdata <= irxdata;
+      orxdv   <= irxdv and irx_ena;
       orxerr  <= irxdv and irxer;
+    end if;
+    end if;
+  end process;
+
+  --! create enables
+  process (iclk, irst_n) is
+  begin
+    if irst_n = '0' then
+      dataen   <= '0';
+      crcen2   <= '0';
+    elsif rising_edge(iclk) then
+      -- enable for sending data out
+      if state = idle or state = sfd then
+        dataen   <= '0';
+      else
+        dataen   <= '1';
+      end if;
+      -- enable for checksum
+      crcen2  <= irx_ena and irxdv and crcen  and not irxer;
     end if;
   end process;
 
@@ -88,11 +104,14 @@ begin
   process (iclk, irst_n) is
   begin
     if irst_n = '0' then
-      dvdly      <= (others => '0');
       datadly    <= (others => (others => '0'));
+      crc_calculated(crc_calculated'high downto 1) <= (others => (others => '0'));
     elsif rising_edge(iclk) then
+    if irx_ena = '1'then
       datadly    <= datadly(datadly'high-1 downto 0) & irxdata;
-      dvdly      <= dvdly(dvdly'high-1 downto 0) & rxdv;
+      crc_calculated(crc_calculated'high downto 1) <= crc_calculated(crc_calculated'high-1 downto 0);
+    else
+    end if;
     end if;
   end process;
 
@@ -103,7 +122,7 @@ begin
       irst_n  => irst_n,
       iinit   => sof,
       icalcen => crcen2,
-      idata   => irxdata,
+      idata   => datadly(0),
       ocrc    => crc_calculated(0)
     );
 
@@ -111,10 +130,8 @@ begin
   process (iclk, irst_n) is
   begin
     if irst_n = '0' then
-      crc_calculated(crc_calculated'high downto 1) <= (others => (others => '0'));
       crc_is_fine       <= '0';
     elsif rising_edge(iclk) then
-      crc_calculated(crc_calculated'high downto 1) <= crc_calculated(crc_calculated'high-1 downto 0);
       if  datadly(3) = crc_calculated(crc_calculated'high)(8*4-1 downto 8*4-8) and
           datadly(2) = crc_calculated(crc_calculated'high)(8*3-1 downto 8*3-8) and
           datadly(1) = crc_calculated(crc_calculated'high)(8*2-1 downto 8*2-8) and
@@ -127,7 +144,7 @@ begin
   end process;
 
 
-
+  --! recieve controller
   process (iclk, irst_n) is
     variable ethtype : std_logic_vector(15 downto 0);
   begin
@@ -139,13 +156,13 @@ begin
       mac_match    <= '0';
       crcen        <= '0';
       sof          <= '0';
-      dataen       <= '0';
       destmacaddr  <= (others => '0');
       olenerr      <= '0';
       ochecksumerr <= '0';
       osof         <= '0';
     elsif rising_edge(iclk) then
       eof  <= '0';
+    if irx_ena = '1'then
       sof  <= '0';
       osof <= '0';
 
@@ -153,7 +170,6 @@ begin
         -----------------------------------------------------------------------
         when idle =>
           crcen        <= '0';
-          dataen       <= '0';
           mac_match    <= '0';
           olenerr      <= '0';
           ochecksumerr <= '0';
@@ -171,7 +187,6 @@ begin
             if bytecnt(2 downto 0) = b"110" then
               state   <= dest_mac;
               crcen   <= '1';
-              dataen  <= '1';
               osof    <= '1';
               bytecnt <= (others => '0');
             else
@@ -242,7 +257,6 @@ begin
             if bytecnt(0) = '1' then
               bytecnt(1 downto 0) <= (others => '0');
               state   <= payload;
-              dataen  <= '1';
               -- depending on the standard either type or length is in these bytes
               opayloadlen <= unsigned(ethtype);
             end if;
@@ -258,9 +272,12 @@ begin
             bytecnt <= bytecnt + 1;
           end if;
           -- an IPG is transmitted before the next frame, already move to idle again
-          if irxdv = '0' then
+          if irxdv = '0'then
             state         <= idle;
-            dataen        <= '0';
+            eof           <= '1';
+            ochecksumerr  <= crc_is_fine;
+          elsif irx_lst = '1' then
+            state         <= idle;
             eof           <= '1';
             ochecksumerr  <= crc_is_fine;
           end if;
@@ -269,6 +286,7 @@ begin
           state <= idle;
       end case;
 
+    end if;
     end if;
   end process;
 
