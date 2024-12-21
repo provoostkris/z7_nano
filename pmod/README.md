fe081f4ee213da012a711b74b5aa5635f1402be2
Kris Provoost <provoostkris@hotmail.com>
2024-12-21 17:00:45 +0100

    adding build script

--- pmod/README.md
+++ pmod/README.md
@@ -4,12 +4,14 @@
 Design containing sevaral simple cores to interface zith common PMOD boards>
 A PMOD board is a very simplistic interface and some basic function , to add to your FPGA>
 PMOD support is forseen for
-- PMOD-LEDx8 : 8 LED s 
-- PMOD-DTx2  : 7 SEG display two digits
-- PMOD-DVI   : display interface
-- PMOD-BTNx4 : 4 push button and 4 slide button 
-- PMOD-TFCARD: memory card interface
-- PMOD-LCD   : display module
+- [x] PMOD-LEDx8 : [8 LED s](https://wiki.sipeed.com/hardware/en/tang/tang-PMOD/FPGA_PMOD.html#PMOD_LEDx8)
+- [x] PMOD-DTx2  : [7 SEG display two digits](https://wiki.sipeed.com/hardware/en/tang/tang-PMOD/FPGA_PMOD.html#PMOD_DTx2)
+- []  PMOD-DVI   : [display interface](https://wiki.sipeed.com/hardware/en/tang/tang-PMOD/FPGA_PMOD.html#PMOD_DVI)
+- []  PMOD-BTNx4 : [4 push button and 4 slide button](https://wiki.sipeed.com/hardware/en/tang/tang-PMOD/FPGA_PMOD.html#PMOD_BTN4%2B4)
+- [] PMOD-TFCARD: [memory card interface](https://wiki.sipeed.com/hardware/en/tang/tang-PMOD/FPGA_PMOD.html#PMOD_TF-CARD)
+- [] PMOD-LCD   : [display module](https://www.tindie.com/products/johnnywu/pmod-lcd-096-expansion-board/)
 
 ### [I/O](constraints)
+The PL push button is the reset
+The clock is directly from the on board crystal
 
---
+++ pmod/scripts/vivado/build.tcl
@@ -0,0 +1,48 @@
+# Typical usage: vivado -mode tcl -source build.tcl
+
+set loc_script [file normalize [info script]]
+set loc_folder [file dirname $loc_script]
+puts $loc_folder
+cd $loc_folder
+
+set CompilationStart [clock seconds]
+
+# Create the project and directory structure
+create_project -force led_blink ./output -part xc7z020clg400-2
+
+set_property target_language VHDL [current_project]
+set_property simulator_language VHDL [current_project]
+
+#
+# Add various sources to the project
+set fp [open ../../rtl.txt]
+while {-1 != [gets $fp line]} {
+    set file_src ../../$line
+    puts $file_src
+    add_files -norecurse $file_src
+}
+
+#
+# Add various constraints to the project
+add_files ../../constraints/pin_constraints.xdc
+    
+#
+# Update to set top and file compile order
+update_compile_order -fileset sources_1
+
+#
+# Launch Synthesis
+launch_runs synth_1
+wait_on_run synth_1
+open_run synth_1 -name netlist_1
+
+#
+# Launch Implementation
+launch_runs impl_1 -to_step write_bitstream
+wait_on_run impl_1
+
+
+set duration [expr [clock seconds]-$CompilationStart]
+set LastCompilationTime $CompilationStart
+puts [format "Compilation duration: %d:%02d" [expr $duration/60] [expr $duration%60]]
+puts [clock format $LastCompilationTime -format {Reporting last compilation time: %A, %d of %B, %Y - %H:%M:%S}]
